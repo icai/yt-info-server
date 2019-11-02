bddcb9f129bdc0d8058755dfa17ecdda1e72d34c
Terry Cai <gidcai@gmail.com>
2019-11-02 22:42:35 +0800

    fix bug

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,11 @@
-function init {
+DOMAIN = api-yt.w3cub.com
+EMAIL = gidcai@gmail.com
+
+
+init() {
     apt-get update
+
+
     # # install nginx
     if ! nginx -t 2>/dev/null; then 
         apt-get install -y nginx
@@ -33,7 +39,7 @@ function init {
         echo "no found  ecosystem.config.js"
     fi
 
-    cp ./ytinfo.conf /etc/nginx/conf.d/ytinfo.conf
+    # cp ./ytinfo.conf /etc/nginx/conf.d/ytinfo.conf
 
     # config hosts
     if ! grep -q "127.0.0.1	api-yt.w3cub.com" /etc/hosts; then 
@@ -41,11 +47,50 @@ function init {
     fi
 
 
-    # setup firewall
 
-    iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 80 -j ACCEPT
 
-    service nginx restart
+    
+}
+
+
+
+config_nginx() {
+    # install certbot
+    apt-get install dirmngr
+    apt-get install software-properties-common -y
+    add-apt-repository ppa:certbot/certbot -y
+    apt-get update
+    apt-get install python-certbot-nginx -y
+
+    # certbot certonly --nginx --agree-tos -n -d $DOMAIN --email $EMAIL
+# Update nginx config to enable https
+cat << EOF>/etc/nginx/conf.d/$DOMAIN.conf
+server {
+    server_name $DOMAIN;
+    location / {
+        proxy_pass http://localhost:3000;
+        proxy_set_header X-Real-IP \$remote_addr;
+    }
+    listen 443 ssl; # managed by Certbot
+    ssl_certificate /etc/letsencrypt/live/$DOMAIN/fullchain.pem; # managed by Certbot
+    ssl_certificate_key /etc/letsencrypt/live/$DOMAIN/privkey.pem; # managed by Certbot
+    include /etc/letsencrypt/options-ssl-nginx.conf; # managed by Certbot
+    ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem; # managed by Certbot
+}
+server {
+    listen          80;
+    server_name     $DOMAIN;
+    return          301 https://\$server_name\$request_uri;
+}
+EOF
+
+service nginx restart
+
+# setup firewall
+
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 80 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 443 -j ACCEPT
+
 }
 
 if [[ $(id -u) -ne 0 ]]; then
@@ -55,6 +100,7 @@ if [[ $(id -u) -ne 0 ]]; then
 fi
 
 init
+config_nginx
 
 # copy nginx config
 
